7a644e583ce1f8218e361da7460a8c927e7fb209
Forget Guillaume <9430@holbertonstudents.com>
2024-09-04 13:15:19 +0200

    return to base for better comprehension of checkers failure

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if a == 0 and b < 0:
-        return 0
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-    for _ in range(b):
-        result *= a
-    return result
+    return a ** b
