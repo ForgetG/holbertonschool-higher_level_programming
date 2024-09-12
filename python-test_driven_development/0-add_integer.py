7601c8ac2b473c61b8742ba3644109d31402fa03
Forget Guillaume <9430@holbertonstudents.com>
2024-09-12 10:27:42 +0200

    remove excess error handling

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,14 +20,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float) and (a > 1e308 or a < -1e308):
-        raise OverflowError("a is too large")
-    if isinstance(b, float) and (b > 1e308 or b < -1e308):
-        raise OverflowError("b is too large")
-    if len(str(a)) > 1000:
-        raise ValueError("a is too long")
-    if len(str(b)) > 1000:
-        raise ValueError("b is too large")
     a = int(a)
     b = int(b)
     return a + b
