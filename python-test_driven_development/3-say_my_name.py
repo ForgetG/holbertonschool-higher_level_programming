ee1546cc7a1810b4ecc2af20b3da4c6432d4cedc
ForgetG <g.forget1999@gmail.com>
2024-09-12 15:00:20 +0200

    added task 2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Function say_my_name prints "My name is <first name> <last name>".
+
+Checks if the type of first_name and last_name is string.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>".
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
