9e317572743f72902977be00dfeabdb31ef99b43
Forget Guillaume <9430@holbertonstudents.com>
2024-09-16 11:08:02 +0200

    added task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    ''' class that defines a square'''
+    def __init__(self, size):
+        '''
+        Initialisation that store the size of square
+
+        Args: size (int): size of square
+        '''
+        self.__size = size
