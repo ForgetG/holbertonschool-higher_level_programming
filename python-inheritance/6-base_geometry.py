283351c9bbebdf6b196d6903f4dc7f8086ff3d2f
Forget Guillaume <9430@holbertonstudents.com>
2024-09-23 13:01:52 +0200

    added task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module 6-base_geometry'''
+
+
+class BaseGeometry():
+    '''BaseGeometry Class'''
+    def area(self):
+        '''
+        Public instance method that raises an Exception with message
+        '''
+        raise Exception("area() is not implemented")
