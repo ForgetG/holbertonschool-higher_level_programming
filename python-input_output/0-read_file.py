bedf983e001895ee8e205caa875e4dab1fda4063
Forget Guillaume <9430@holbertonstudents.com>
2024-09-30 14:08:04 +0200

    added function documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,8 @@
 
 
 def read_file(filename=""):
+    '''Function to read a text file and prints it to stdout'''
+
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data, end="")
