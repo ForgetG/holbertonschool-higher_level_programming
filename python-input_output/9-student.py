933dfad016b81a207cdfb1198fa1b5436a4b8714
Forget Guillaume <9430@holbertonstudents.com>
2024-09-30 16:53:28 +0200

    added task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module 9-student'''
+import json
+
+
+class Student:
+    '''
+    class Student defined by:
+        first_name
+        last_name
+        age
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation of Student class'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        Public method that retrieves
+        a dictionnary representation of Student instance
+        '''
+        return self.__dict__
