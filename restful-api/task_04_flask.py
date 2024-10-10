3bbfe0336484c8479afbd16e17f0be6b26cffecb
Forget Guillaume <9430@holbertonstudents.com>
2024-10-10 15:15:41 +0200

    imported json

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Module task_04_flask"""
 from flask import Flask, jsonify, request
+import json
 
 
 app = Flask(__name__)
 
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"name": "John", "age": 30, "city": "New York"}
-    }
+users = {}
 
 
 @app.route("/")
