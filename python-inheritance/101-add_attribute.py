4b9a3fde0ae51174e29a3e2e123048e8c97d6d28
Forget Guillaume <9430@holbertonstudents.com>
2024-10-14 17:09:07 +0200

    added shebang

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Module 101-add_attribute"""
+
+
 def add_attribute(obj, attr_name, attr_value):
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
