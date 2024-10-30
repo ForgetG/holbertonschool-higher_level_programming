be7172867da9aaace8b2af0f97da8ebcf2fc5385
ForgetG <g.forget1999@gmail.com>
2024-10-30 09:58:43 +0100

    added ;

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of California
 SELECT * FROM cities 
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California');
