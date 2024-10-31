b13bcbe9a09b002246e429cfb34768b57bd79251
ForgetG <g.forget1999@gmail.com>
2024-10-31 14:25:47 +0100

    modify * for specified SELECT FROM

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = (
-        "SELECT * FROM cities "
+        "SELECT cities.id, cities.name, states.name FROM cities "
         "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
     )
     cursor.execute(query)
